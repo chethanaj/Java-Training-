9e0797a594cf85ee6132564997eb10da7048e65a
Chethana <chethanad.jayarathne@gmail.com>
2020-01-09 23:15:30 +0530

    Add AllUpperCase and ReverseNumbers files.

---
+++ AllUpperCase/Application.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+        UpperCaseReader upperCaseReader = new UpperCaseReader(new File("/home/chethana/Documents/Sample.txt"));
+        int i;
+        while ((i = upperCaseReader.read()) != -1)
+            System.out.print((char) i);
+        upperCaseReader.close();
+
+
+    }
+
+}
---
+++ ReverseNumbers/README.md
@@ -0,0 +1,14 @@
+Reverse a given input
+________________________
+
+Take user input as command line argument and print it reverse order.
+
+* Store args[0]'s value as a String. Because in main() takes command line arguments and store it in a String array. 
+
+* Convert that String into byte[]. 
+
+* Create byte[] for store the result.
+
+* for loop store the reverse order of the given input. 
+
+* Print the result. 
