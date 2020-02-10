b59bcda8dc3a7d924ac8322769a86e3d2a8445f7
Chethana <chethanad.jayarathne@gmail.com>
2020-02-10 23:28:45 +0530

    Add oneToOne,OneToMany and ManyToMany mappings.

---
+++ SpringProjects/employee-serviceWithOneToOne/src/main/java/com/example/employeeservice/service/EmployeeServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.employeeservice.service;
+
+import com.example.employeeservice.model.Employee;
+import com.example.employeeservice.model.Telephone;
+import com.example.employeeservice.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public Employee save(Employee employee) {
+
+
+        for (Telephone telephone:employee.getTelephones()) {
+            telephone.setEmployee(employee);
+
+        }
+
+
+        return employeeRepository.save(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+}
